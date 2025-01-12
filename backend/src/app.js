71e373f9f492d770ca0521536df4c434fc0f3b4d
Nandainthegrass <postnanda200@gmail.com>
2025-01-12 21:02:21 +0530

    Logger/Frontend setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes'); // Import all routes
 const { errorHandler } = require('./middlewares/errorHandler');
+const logger = require('./utils/logger');
 
 const app = express();
 
@@ -8,8 +9,10 @@ const app = express();
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
+app.use(logger);
 // Routes
 app.use('/api', routes); // Prefix all routes with /api
+
 
 // Error Handling
 app.use(errorHandler);
--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -1,12 +1,20 @@
-const winston = require('winston');
+// const winston = require('winston');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'app.log' }),
-  ],
-});
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({ filename: 'app.log' }),
+//   ],
+// });
+
+const logger = (req, res, next) => {
+  const curTime  = new Date(Date.now());
+  const formattedDate = curTime.toLocaleString('en-IN', {timeZone: 'Asia/Kolkata'});
+  console.log("Method:", req.method, "    ", "URL:", req.url,"     ", "Time:", formattedDate);
+  next();
+}
+
 
 module.exports = logger;
