d7b68b9beb42386428cc9f9d67e1cabbcb9f142a
Nandainthegrass <postnanda200@gmail.com>
2025-01-21 20:16:36 +0530

    layout

--- frontend/src/pages/DashboardLayout.jsx
+++ frontend/src/pages/DashboardLayout.jsx
@@ -8,14 +8,17 @@ const DashboardLayout = ({children}) => {
                 <h3 className='app-name'>DocIQ</h3>
             </div>
             <div className='nav-and-main'>
-                <nav className='dashboard-nav'>
-                    <ul>
-                        <li>Home</li>
-                        <li>Contact</li>
-                        <li>Forms</li>
-                        <li>About</li>
-                    </ul>
-                </nav>
+                <div className='nav-container'>
+                    <nav className='dashboard-nav'>
+                        <ul>
+                            <li>Home</li>
+                            <li>Contact</li>
+                            <li>Forms</li>
+                            <li>About</li>
+                        </ul>
+                    </nav>
+                </div>
+                
                 <div className='main-content'></div>
             </div>
         </div>
