98b664880f81f36d4bffec6c6c3095cb4479149a
Winston-Dmello <dmellowinston@gmail.com>
2025-01-22 10:55:01 +0530

    some stupid design layout

--- frontend/src/pages/DashboardLayout.jsx
+++ frontend/src/pages/DashboardLayout.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import './DashboardLayout.css';
+import dociq_logo from '../assets/dociq_logo.png';
 
 const DashboardLayout = ({children}) => {
     return <>
         <div className='dashboard-body'>
             <div className='side-bar'>
-                <h3 className='app-name'>DocIQ</h3>
+                <h3 className='app-name'><img className='dociq-logo' src={dociq_logo} alt='DocIQ Logo'/>DocIQ</h3>
             </div>
             <div className='nav-and-main'>
                 <div className='nav-container'>
