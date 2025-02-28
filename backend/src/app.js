1ea5f227cad090d099a944284b464330b711bca3
Nandainthegrass <postnanda200@gmail.com>
2025-02-28 18:45:01 +0530

    path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 const { errorHandler } = require('./middlewares/errorHandler');
 const logger = require('./middlewares/logger');
 
