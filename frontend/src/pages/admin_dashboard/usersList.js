9cb8ff58b363f91b0afcc189ec9397030a51fcc9
Winston-Dmello <dmellowinston@gmail.com>
2025-03-15 11:45:28 +0530

    pakka will cry

--- frontend/src/pages/admin_dashboard/usersList.js
+++ frontend/src/pages/admin_dashboard/usersList.js
@@ -1,27 +1,21 @@
-import { useContext } from 'react';
-import JWTContext from "../../contexts/jwtcontext";
-
-// Custom hook to use auth functions
-const useAuth = () => useContext(JWTContext);
-
 const getUsers = async () => {
-    const { authFetch } = useAuth();
-    
     try{
-        const response = await authFetch('/users', {
-            method: 'GET'
-        });
-        
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem("token")
+            }
+        })
         if(response.ok){
             const users = await response.json();
             return users;
         }else{
             return {type:"error", message: "Error Fetching Users"};
         }
-    }catch(error){
+    }catch{
         return {type: "error", message: "Error Connecting to Server"};
     }
 }
 
-export {getUsers, useAuth};
-
+export {getUsers};
